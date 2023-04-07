ce5c1b2a63e94271496fdd220eed6ff2309df73a
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-07 11:41:21 -0700

    Add cost function to object to output topping, size, cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 // Business Logic
-function Pie(topping, size) {
+
+function Pie(topping, size, cost) {
   this.topping = topping;
   this.size = size;
+  this.cost = 10;
 }
 
+Pie.prototype.cost = function() {
+  let cost = 10;
+  if (this.topping === "Ricotta" && this.size === "Small") {
+    cost + 3;
+  }
+    return cost;
+};
 
-
-
-
-
-
-
-
-
+Pie.prototype.customerPie = function() {
+  return this.topping + " " + this.size + this.cost;
+};
 
 
 
@@ -35,3 +39,15 @@ function Pie(topping, size) {
 
 
 // UI Logic
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+  //let newPie= new Pie(topping, size);
+
+}
+
+
+
+window.addEventListener("load", function() {
+  document.getElementById("pizza-pie").addEventListener("submit", handleFormSubmission);
+});
